2463934078f421a03f9f395b1db31536a60c4310
Mateusz Kowalczyk <mkowalczyk013@gmail.com>
2022-10-17 17:39:31 +0200

    Add alien fleet.

--- alien_invasion.py
+++ alien_invasion.py
@@ -19,6 +19,7 @@ class AlienInvasion():
         ''' Game initialization. '''
         pygame.init()  # Initialization of screen.
         pygame.display.set_caption('Alien Invasion')
+
         self.settings: Settings = Settings()
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
@@ -26,9 +27,11 @@ class AlienInvasion():
         # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         # self.settings.screen_width = self.screen.get_rect().width
         # self.settings.screen_height = self.screen.get_rect().height
+
         self.ship: Spaceship = Spaceship(self)
         self.bullets = pygame.sprite.Group()
         self.aliens = pygame.sprite.Group()
+
         self._create_fleet()
 
     def run_game(self) -> None:
@@ -84,6 +87,29 @@ class AlienInvasion():
     def _create_fleet(self) -> None:
         ''' Create new alien fleet. '''
         alien: Alien = Alien(self)
+
+        alien_width: int = alien.rect.width  # type: ignore
+        available_space_x: int = self.settings.screen_width - (2*alien_width)
+        number_aliens_x: int = available_space_x // (2*alien_width)
+
+        alien_height: int = alien.rect.height  # type: ignore
+        ship_height: int = self.ship.rect.height
+        available_space_y: int = self.settings.screen_height - (3*alien_height) - ship_height
+        number_rows: int = available_space_y // (2*alien_height)
+
+        # Create the fleet
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number: int, row_number: int) -> None:
+        ''' Create new alien and add it to the row. '''
+        alien: Alien = Alien(self)
+        alien_width: int = alien.rect.width  # type: ignore
+        alien_height: int = alien.rect.height  # type: ignore
+        alien.x = alien_width + 2*alien_width*alien_number
+        alien.rect.x = alien.x  # type: ignore
+        alien.rect.y = alien.rect.height + 2*alien_height*row_number  # type: ignore
         self.aliens.add(alien)
 
     def _update_screen(self) -> None:
